51e5ab110cbebb91f48d7ee6e765299bf332f1ee
profe-elisachang <elisachangn@gmail.com>
2026-02-08 16:51:44 -0600

    Add new article: 🖊️ The Life-Saving Secret behind the Hole in Pen Caps

--- script.js
+++ script.js
@@ -66,7 +66,8 @@ async function scanLessons() {
         { filename: 'The Uplifting Aroma of Scented Candles.html', date: '2026-02-09', title: '🕯️ The Uplifting Aroma of Scented Candles' },
         { filename: 'The Science of Memory.html', date: '2026-02-06', title: '🧠 The Science of Memory: Why We Remember and Why We Forget' },
         { date: '2026-02-02', title: 'Flexible Adjustment Holiday:Día de la Constitución Mexicana - No Class', isHoliday: true }
-        // 👆 在此上方添加新文章，記得加逗號！
+                { filename: 'Pen Caps.html', date: '2026-02-13', title: '🖊️ The Life-Saving Secret behind the Hole in Pen Caps' },
+// 👆 在此上方添加新文章，記得加逗號！
         // 格式：{ filename: '檔名.html', date: 'YYYY-MM-DD', title: '標題（含emoji）' }
         // 假日通知範例：{ date: '2026-12-25', title: '🎄 Christmas - No Class', isHoliday: true }
     ];
