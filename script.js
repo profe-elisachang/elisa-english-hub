133d35619d0e6dddb9effe53af64e1d386c50a21
profe-elisachang <elisachangn@gmail.com>
2026-02-09 13:58:41 -0600

    Add new article: 🕯️ The Uplifting Aroma of Scented Candles

--- script.js
+++ script.js
@@ -71,6 +71,7 @@ async function scanLessons() {
         { filename: 'Pen Caps.html', date: '2026-02-13', title: '🖊️ The Life-Saving Secret behind the Hole in Pen Caps' },
         { filename: 'The Uplifting Aroma of Scented Candles.html', date: '2026-02-09', title: '🕯️ The Uplifting Aroma of Scented Candles' },
         { filename: 'The Uplifting Aroma of Scented Candles.html', date: '2026-02-09', title: '🕯️ The Uplifting Aroma of Scented Candles' },
+        { filename: 'The Uplifting Aroma of Scented Candles.html', date: '2026-02-09', title: '🕯️ The Uplifting Aroma of Scented Candles' },
 // 👆 在此上方添加新文章，記得加逗號！
         // 格式：{ filename: '檔名.html', date: 'YYYY-MM-DD', title: '標題（含emoji）' }
         // 假日通知範例：{ date: '2026-12-25', title: '🎄 Christmas - No Class', isHoliday: true }
