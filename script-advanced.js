755e82e80167a6b7ddf85e18cd0d0def24a4acad
profe-elisachang <elisachangn@gmail.com>
2026-04-07 10:54:41 -0600

    Update script-advanced.js: Developing Countries Are Being Priced Out, in Struggle for Affordable Finance

--- script-advanced.js
+++ script-advanced.js
@@ -85,6 +85,7 @@ async function scanLessons() {
         { filename: 'Growing Muscles.html', date: '2026-03-26', title: '💪 Growing Muscles, Growing Markets' },        { filename: 'Hero Tax.html', date: '2026-03-31', title: '🦸 The Hero Tax: Why \'Selfless\' Workers Are Professionally Exploited' },
         { filename: 'Persian Gulf Crisis.html', date: '2026-04-02', title: 'Persian Gulf Crisis Impacting Food Security' },
         { filename: 'Crisis en el Golfo Pérsico.html', date: '2026-04-02', title: 'Crisis en el Golfo Pérsico' },
+        { filename: 'Developing Countries Are Being Priced Out.html', date: '2026-04-07', title: 'Developing Countries Are Being Priced Out, in Struggle for Affordable Finance' },
         // 👆 在此上方添加新文章，記得加逗號！
         // 格式：{ filename: '檔名.html', date: 'YYYY-MM-DD', title: '標題（含emoji）' }
         // 假日通知範例：{ date: '2026-12-25', title: '🎄 Christmas - No Class', isHoliday: true }
