9927ff4e410834b3f717493dbd3fc38a488fc828
profe-elisachang <elisachangn@gmail.com>
2026-05-08 11:50:46 -0600

    Update script.js: Adjust lesson schedule and add holiday entries

--- script.js
+++ script.js
@@ -108,10 +108,10 @@ async function scanLessons() {
         { filename: 'upskill your comunication.html', date: '2026-04-27', title: 'No, you can\'t upskill your culture. But you can upskill your leaders ' },
         { date: '2026-05-01', title: 'Labor Day – No class', isHoliday: true },
         { filename: 'India\'s Cash Transfers.html', date: '2026-04-29', title: '💸 India\'s Cash Transfers Recognize Housewives\' Hidden Labor' },       
-        { filename: 'The Evolution of the Modern Workweek.html', date: '2026-05-04', title: '🏭 The Evolution of the Modern Workweek' },
-        { filename: 'The Evolution of the Modern Workweek.html', date: '2026-05-06', title: '🏭 The Evolution of the Modern Workweek' },
+        { date: '2026-05-04', title: '🏖️ Teacher Off - No Class', isHoliday: true },
+        { filename: 'The Evolution of the Modern Workweek.html', date: '2026-05-08', title: '🏭 The Evolution of the Modern Workweek' },
         { filename: 'The Return of Retro Aesthetics.html', date: '2026-05-06', title: '🕶️ The Return of Retro Aesthetics' },
-        { filename: 'Modern Workweek-Practice.html', date: '2026-05-06', title: '🏭  Modern Workweek-Practice' },
+        { filename: 'Modern Workweek-Practice.html', date: '2026-05-08', title: '🏭  Modern Workweek-Practice' },
 // 👆 在此上方添加新文章，記得加逗號！
         // 格式：{ filename: '檔名.html', date: 'YYYY-MM-DD', title: '標題（含emoji）' }
         // 假日通知範例：{ date: '2026-12-25', title: '🎄 Christmas - No Class', isHoliday: true }
