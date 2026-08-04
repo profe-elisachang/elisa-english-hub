b3dce3623619ccacaa1f3b104feb0b8b17e7fadc
profe-elisachang <elisachangn@gmail.com>
2026-08-04 12:52:24 -0600

    Update script.js: 🧬🫀🔬 Heartening News

--- script.js
+++ script.js
@@ -143,6 +143,7 @@ async function scanLessons() {
         { filename: 'The Right Way to Motivate.html', date: '2026-07-06', title: '🤝 The Right Way to Motivate' },
         { filename: 'Connection, Not Control.html', date: '2026-07-08', title: 'Connection, Not Control🚫' },
         { filename: 'A Hotel Made of Modules.html', date: '2026-07-10', title: '🏜️ A Hotel Made of Modules' },
+        { filename: 'Heartening News.html', date: '2026-08-04', title: '🧬🫀🔬 Heartening News' },
 // 👆 在此上方添加新文章，記得加逗號！
         // 格式：{ filename: '檔名.html', date: 'YYYY-MM-DD', title: '標題（含emoji）' }
         // 假日通知範例：{ date: '2026-12-25', title: '🎄 Christmas - No Class', isHoliday: true }
