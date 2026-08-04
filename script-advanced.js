503d8614158a28ca38fcf7d1e2174a678ac818c6
profe-elisachang <elisachangn@gmail.com>
2026-08-04 15:12:40 -0600

    🧬🫀🔬 Heartening News

--- script-advanced.js
+++ script-advanced.js
@@ -120,6 +120,8 @@ async function scanLessons() {
         { filename: 'Spotify Unwrapped.html', date: '2026-07-30', title: '' },
         { filename: 'Spotify.html', date: '2026-07-30', title: '🎧 Spotify: From Stockholm to the World' },
         { filename: 'Lab-Grown Organs.html', date: '2026-08-04', title: '🫀🔬 Lab-Grown Organs: How Far Should Science Go?' },
+        { filename: 'Lab-Grown Organs.html', date: '2026-08-04', title: '🧬🫀🔬
+Heartening News' },
 // 👆 在此上方添加新文章，記得加逗號！
         // 格式：{ filename: '檔名.html', date: 'YYYY-MM-DD', title: '標題（含emoji）' }
         // 假日通知範例：{ date: '2026-12-25', title: '🎄 Christmas - No Class', isHoliday: true }
